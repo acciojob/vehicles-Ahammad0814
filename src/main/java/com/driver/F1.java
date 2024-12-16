a6779773182cf0497837a96abb2ed59ea00bb23e
Ahammad Ali Pinjari <ahammad0814@gmail.com>
2024-12-16 07:47:26 +0530

    Update F1.java

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,11 +4,12 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
-        super(name, isManual);
+        super(name, 4, isManual, 0, 0);
     }
 
     public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+        int newSpeed = getCurrentSpeed() + rate;
+        //set the value of new speed by using currentSpeed and rate
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -18,10 +19,31 @@ public class F1 extends Car {
          * speed 201-250: gear 5
          * speed more than 250: gear 6
          */
+        if (newSpeed <= 0) {
+            // Stop the car, set the speed to 0 and gear to 1
+            stop(); // Assuming stop() sets current speed to 0
+            changeGear(1);
+            System.out.println("Car has stopped. Gear set to 1.");
+        } else {
+            // Update gear based on the speed ranges
+            if (newSpeed >= 1 && newSpeed <= 50) {
+                changeGear(1);
+            } else if (newSpeed >= 51 && newSpeed <= 100) {
+                changeGear(2);
+            } else if (newSpeed >= 101 && newSpeed <= 150) {
+                changeGear(3);
+            } else if (newSpeed >= 151 && newSpeed <= 200) {
+                changeGear(4);
+            } else if (newSpeed >= 201 && newSpeed <= 250) {
+                changeGear(5);
+            } else if (newSpeed > 250) {
+                changeGear(6);
+            }
 
-        if(newSpeed == 0) {
-            //Stop the car, set gear as 1
-        }
+        // if(newSpeed == 0) {
+        //     changeGear(1);
+        //     //Stop the car, set gear as 1
+        // }
         //for all other cases, change the gear accordingly
 
         if(newSpeed > 0) {
