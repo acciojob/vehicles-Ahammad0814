55d89b3e35a220f4cba9150881a0e72b91f3d0c6
Ahammad Ali Pinjari <ahammad0814@gmail.com>
2024-12-26 22:44:14 +0530

    Update Vehicle.java

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -1,31 +1,29 @@
 package com.driver;
 
 public class Vehicle {
+    protected String name;
+    protected int capacity;
 
-    private String name;
-    private int currentSpeed;
-    private int currentDirection;
-
-    public Vehicle(String name) {
+    // Constructor
+    public Vehicle(String name, int capacity) {
         this.name = name;
+        this.capacity = capacity;
     }
 
-    public void steer(int direction){
-        this.currentDirection = direction;
-        //direction is in degrees, add it to the current direction
-        System.out.println("steer method called - The direction is changed to: " + currentDirection + " degrees");
+    // Getter for vehicle name
+    public String getVehicleName() {
+        return name;
     }
 
-    public void move(int speed, int direction){
-        this.currentSpeed = speed;
-        this.currentDirection = direction;
-        //set the values of currentSpeed and currentDirection
-        System.out.println("move method called - The speed is changed to: " + currentSpeed + ", and the direction is changed to: " + currentDirection + " degrees");
+    // Getter for vehicle capacity
+    public int getVehicleCapacity() {
+        return capacity;
     }
 
-    public void stop(){
-
-        System.out.println("stop method called - The vehicle is stopped");
+    // A generic method to display vehicle info
+    public void displayInfo() {
+        System.out.println("Vehicle Name: " + name);
+        System.out.println("Vehicle Capacity: " + capacity);
     }
-
 }
+
