f0bc583897b8b9c62ced4d461ce793be900b58cb
Ahammad Ali Pinjari <ahammad0814@gmail.com>
2024-12-26 22:44:38 +0530

    Update Car.java

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -1,31 +1,13 @@
 package com.driver;
-
 public class Car extends Vehicle {
-    private int wheels;
-    private String type;
-    private int doors;
-    private int gears;
-    private boolean isManual;
-    private int currentGear;
-    private int seats;
 
-    public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
-        super(name);
-        this.wheels = wheels;
-        this.doors = doors;
-        this.gears = gears;
-        this.isManual = isManual;
-        this.type = type;
-        this.seats = seats;
+    // Constructor
+    public Car(String name, int capacity) {
+        super(name, capacity);
     }
 
-    public void changeGear(int newGear){
-        this.currentGear = newGear;
-        System.out.println("changeGear method called - The gear is changed to: " + currentGear);
-    }
-
-    public void changeSpeed(int newSpeed, int newDirection){
-        super(newSpeed, newDirection);
-        System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
+    // Car-specific method (optional)
+    public void displayCarInfo() {
+        System.out.println("Car: " + name + ", Capacity: " + capacity);
     }
 }
