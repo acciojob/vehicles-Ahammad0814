5bad414d332f91bb592792bb4a62a8210d123126
Ahammad Ali Pinjari <ahammad0814@gmail.com>
2024-12-26 22:50:09 +0530

    Update F1.java

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -1,13 +1,35 @@
 package com.driver;
+
+package com.driver;
+
 public class F1 extends Car {
 
-    // Constructor
-    public F1(String name, int capacity) {
-        super(name, capacity);
+    // Constructor to initialize the F1 car
+    public F1(String name, boolean isMoving) {
+        super(name, isMoving);
     }
 
-    // F1-specific method (optional)
-    public void displayF1Info() {
-        System.out.println("F1 Car: " + name + ", Capacity: " + capacity);
+    // Method to accelerate the F1 car (positive or negative acceleration)
+    public void accelerate(int speedIncrease) {
+        if (isMoving) {
+            if (speedIncrease > 0) {
+                System.out.println(name + " accelerated by " + speedIncrease + " km/h.");
+            } else if (speedIncrease < 0) {
+                System.out.println(name + " decelerated by " + (-speedIncrease) + " km/h.");
+            }
+        } else {
+            System.out.println(name + " is not moving, cannot accelerate.");
+        }
+    }
+
+    // Overriding move to provide specific behavior for F1 cars
+    @Override
+    public void move(int speed, int direction) {
+        if (speed > 0) {
+            isMoving = true;
+            System.out.println(name + " is racing at " + speed + " km/h in direction " + direction + " degrees.");
+        } else {
+            stop();
+        }
     }
 }
