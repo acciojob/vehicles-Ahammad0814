3015923a111b5fed3406cc274638c6bdad34cc92
Ahammad Ali Pinjari <ahammad0814@gmail.com>
2024-12-26 22:53:21 +0530

    Update Car.java

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -1,19 +1,70 @@
 package com.driver;
 
 public class Car extends Vehicle {
+    protected int doors;
+    protected int gears;
+    protected int wheels;
+    protected boolean isManual;
+    protected int seats;
+    protected int currentGear;
 
-    // Constructor to initialize the car with name and initial moving state
-    public Car(String name, boolean isMoving) {
-        super(name, isMoving);
+    // Constructor with additional car-specific attributes
+    public Car(String name, int doors, int gears, int wheels, boolean isManual, int seats) {
+        super(name);
+        this.doors = doors;
+        this.gears = gears;
+        this.wheels = wheels;
+        this.isManual = isManual;
+        this.seats = seats;
+        this.currentGear = 0;
     }
 
-    // Method for moving the car
-    public void move(int speed, int direction) {
+    // Getter methods for car-specific attributes
+    public int getDoors() {
+        return doors;
+    }
+
+    public int getGears() {
+        return gears;
+    }
+
+    public int getWheels() {
+        return wheels;
+    }
+
+    public boolean isManual() {
+        return isManual;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public int getCurrentGear() {
+        return currentGear;
+    }
+
+    // Change the car's gear
+    public void changeGear(int gear) {
+        if (gear <= gears && gear >= 0) {
+            currentGear = gear;
+            System.out.println(name + " changed to gear " + gear);
+        }
+    }
+
+    // Change the car's speed
+    public void changeSpeed(int speed) {
         if (speed > 0) {
-            isMoving = true;
-            System.out.println(name + " is moving at " + speed + " km/h in direction " + direction + " degrees.");
+            currentSpeed = speed;
+            System.out.println(name + " is moving at " + speed + " km/h.");
         } else {
             stop();
         }
+    }
+
+    // Method to steer the car
+    public void steer(int direction) {
+        currentDirection = direction;
+        System.out.println(name + " is steering at " + direction + " degrees.");
     }
 }
