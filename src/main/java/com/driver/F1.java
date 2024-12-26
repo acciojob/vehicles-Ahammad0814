e5707c22e00d3ebd4c26422a738556df07860268
Ahammad Ali Pinjari <ahammad0814@gmail.com>
2024-12-26 23:08:28 +0530

    Update F1.java

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -1,30 +1,46 @@
 package com.driver;
 
 public class F1 extends Car {
-    public F1(String name, int speed, int gears, int wheels, boolean isManual, int seats) {
-        super(name, speed, gears, wheels, isManual, seats);
+
+    public F1(String name, boolean isManual) {
+        //Use arbitrary values for parameters which are not mentioned
+        super(name, 4, 4,6, isManual, "racing", 1);
     }
 
-    // Overriding the move method for F1-specific movement
-    @Override
-    public void move(int speed, int direction) {
-        if (speed > 0) {
-            isMoving = true;
-            currentSpeed = speed;
-            currentDirection = direction;
-            System.out.println(name + " is racing at " + speed + " km/h in direction " + direction + " degrees.");
-        } else {
+    public void accelerate(int rate){
+        int newSpeed = getCurrentSpeed() + rate; //set the value of new speed by using currentSpeed and rate
+        /**
+         * speed 0: gear 1
+         * speed 1-50: gear 1
+         * speed 51-100: gear 2
+         * speed 101-150: gear 3
+         * speed 151-200: gear 4
+         * speed 201-250: gear 5
+         * speed more than 250: gear 6
+         */
+
+        if(newSpeed == 0) {
+            //Stop the car, set gear as 1
             stop();
+            changeGear(1);
         }
-    }
-
-    // Accelerate method specific for F1
-    public void accelerate(int speedIncrease) {
-        if (isMoving) {
-            currentSpeed += speedIncrease;
-            System.out.println(name + " accelerated to " + currentSpeed + " km/h.");
-        } else {
-            System.out.println(name + " is not moving, cannot accelerate.");
+        //for all other cases, change the gear accordingly
+        else if (newSpeed <= 50){
+            changeGear(1);
+        } else if (newSpeed <= 100) {
+            changeGear(2);
+        } else if (newSpeed <= 150) {
+            changeGear(3);
+        } else if (newSpeed <= 200) {
+            changeGear(4);
+        } else if (newSpeed <= 250) {
+            changeGear(5);
+        }
+        else {
+            changeGear(6);
+        }
+        if(newSpeed > 0) {
+            changeSpeed(newSpeed, getCurrentDirection());
         }
     }
 }
